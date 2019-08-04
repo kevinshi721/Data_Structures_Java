2f1c52a63f90e6f512d149fbc4d04c2a47e68fdc
kevinshi721 <shi.zho@husky.neu.edu>
2019-08-04 14:09:58 -0400

    LinkedList

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * @author kevinshi721
  * @date 7/28/19 3:57 PM
@@ -5,15 +7,32 @@
 
 public class Main {
 
+    private static double testStack(Stack<Integer> s, int opCount) {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++)
+            s.push(random.nextInt(Integer.MAX_VALUE));
+        for (int i = 0; i < opCount; i++)
+            s.pop();
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+
+    }
+
     public static void main(String[] args) {
 
-        ArrayStack<Integer> stack = new ArrayStack<>();
-        for (int i = 0; i < 5; i++) {
-            stack.push(i);
-            System.out.println(stack);
-        }
+        int opCount = 100000;
 
-        stack.pop();
-        System.out.println(stack);
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time = " + time1 + "s");
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time = " + time2 + "s");
+
     }
 }
