5144bb4af9dc35f20aea09eaa318c567ea7caa17
kevinshi721 <shi.zho@husky.neu.edu>
2019-08-05 22:24:06 -0400

    LeetCode 203

---
+++ LeetCode/203. Remove Linked List Elements/src/ListNode.java
@@ -0,0 +1,37 @@
+/**
+ * @author kevinshi721
+ * @date 8/5/19 8:43 PM
+ */
+public class ListNode {
+
+    int val;
+    ListNode next;
+
+    public ListNode(int x) {
+        val = x;
+    }
+
+    public ListNode(int[] arr) {
+        if(arr == null || arr.length == 0)
+            throw new IllegalArgumentException("arr cannot be null/empty");
+
+        this.val = arr[0];
+        ListNode cur = this;
+        for(int i = 1 ; i < arr.length; i++){
+            cur.next = new ListNode(arr[i]);
+            cur = cur.next;
+        }
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        ListNode cur = this;
+        while(cur.next != null){
+            res.append(cur.val + "->");
+            cur = cur.next;
+        }
+        res.append("null");
+        return  res.toString();
+    }
+}
---
+++ LeetCode/203. Remove Linked List Elements/src/Solution2.java
@@ -0,0 +1,29 @@
+/**
+ * @author kevinshi721
+ * @date 8/5/19 10:03 PM
+ */
+public class Solution2 {
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.val == val)
+                prev.next = prev.next.next;
+            else
+                prev = prev.next;
+        }
+        return dummyHead.next;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 3, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new Solution2()).removeElements(head, 3);
+        System.out.println(res);
+    }
+}
