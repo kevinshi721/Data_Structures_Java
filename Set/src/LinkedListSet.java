3e974cf5f7635ef52fdcb0be22386c6ad6cf8da7
kevinshi721 <shi.zho@husky.neu.edu>
2019-08-17 11:42:06 -0400

    LinkedList and Set

---
+++ Set/src/LinkedListSet.java
@@ -0,0 +1,40 @@
+/**
+ * @author kevinshi721
+ * @date 8/12/19 8:19 PM
+ */
+public class LinkedListSet<E> implements Set<E> {
+
+    private LinkedList<E> list;
+
+    public LinkedListSet(){
+        list = new LinkedList<>();
+    }
+
+
+    @Override
+    public void add(E e) {
+        if (!list.contains(e)){
+            list.addFirst(e);
+        }
+    }
+
+    @Override
+    public void remove(E e) {
+        list.removeElement(e);
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return list.contains(e);
+    }
+
+    @Override
+    public int getSize() {
+        return list.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+}
--- Set/src/Main.java
+++ Set/src/Main.java
@@ -18,18 +18,42 @@ public class Main {
         }
 
         System.out.println();
-
         System.out.println("A Tale of Two Cities");
 
-        ArrayList<String> word2 = new ArrayList<>();
-        if (FileOperation.readFile("a-tale-of-two-cities.txt", word2)){
-            System.out.println("Total words: " + word2.size());
+        ArrayList<String> words2 = new ArrayList<>();
+        if (FileOperation.readFile("a-tale-of-two-cities.txt", words2)){
+            System.out.println("Total words: " + words2.size());
 
             BSTSet<String> set1 = new BSTSet<>();
-            for (String word : word2)
+            for (String word : words2)
                 set1.add(word);
             System.out.println("Total different words: " + set1.getSize());
         }
 
+        System.out.println();
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words3 = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt", words3)){
+            System.out.println("Total words: " + words3.size());
+
+            LinkedListSet<String> set1 = new LinkedListSet<>();
+            for (String word : words3)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
+
+        System.out.println();
+        System.out.println("A Tale of Two Cities");
+
+        ArrayList<String> words4 = new ArrayList<>();
+        if (FileOperation.readFile("a-tale-of-two-cities.txt", words4)){
+            System.out.println("Total words: " + words4.size());
+
+            LinkedListSet<String> set1 = new LinkedListSet<>();
+            for (String word : words4)
+                set1.add(word);
+            System.out.println("Total different words: " + set1.getSize());
+        }
     }
 }
