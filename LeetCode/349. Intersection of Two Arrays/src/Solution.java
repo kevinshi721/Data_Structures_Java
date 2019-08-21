47a65a5035a75847d1d8c9e126f971ccc254466f
kevinshi721 <shi.zho@husky.neu.edu>
2019-08-20 21:29:41 -0400

    350. Intersection of Two Arrays II

--- LeetCode/349. Intersection of Two Arrays/src/Solution.java
+++ LeetCode/349. Intersection of Two Arrays/src/Solution.java
@@ -9,12 +9,12 @@ public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         TreeSet<Integer> treeSet = new TreeSet<>();
 
-        for(int num : nums1)
+        for (int num : nums1)
             treeSet.add(num);
 
         ArrayList<Integer> list = new ArrayList<>();
 
-        for(int num : nums2) {
+        for (int num : nums2) {
             if (treeSet.contains(num)) {
                 list.add(num);
                 treeSet.remove(num);
@@ -22,7 +22,7 @@ public class Solution {
         }
 
         int[] res = new int[list.size()];
-        for(int i = 0 ; i < list.size(); i++)
+        for (int i = 0; i < list.size(); i++)
             res[i] = list.get(i);
 
         return res;
