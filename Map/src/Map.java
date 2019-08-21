1a50a800166156340960dcbd20f01da12181411a
kevinshi721 <shi.zho@husky.neu.edu>
2019-08-20 20:45:51 -0400

    Map

---
+++ Map/src/Map.java
@@ -0,0 +1,21 @@
+/**
+ * @author kevinshi721
+ * @date 8/18/19 1:27 PM
+ */
+public interface Map<K, V> {
+
+    void add(K key, V value);
+
+    V remove(K key);
+
+    boolean contains(K key);
+
+    V get(K key);
+
+    void set(K key, V newValue);
+
+    int getSize();
+
+    boolean isEmpty();
+
+}
