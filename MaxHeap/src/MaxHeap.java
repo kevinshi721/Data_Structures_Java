cf3786154abb1e30b216b849ef7e634b87efba0a
kevinshi721 <shi.zho@husky.neu.edu>
2019-08-21 23:19:53 -0400

    MaxHeap

---
+++ MaxHeap/src/MaxHeap.java
@@ -0,0 +1,38 @@
+/**
+ * @author kevinshi721
+ * @date 8/21/19 11:05 PM
+ */
+public class MaxHeap<E extends Comparable<E>> {
+
+    private Array<E> data;
+
+    public MaxHeap(int capacity) {
+        data = new Array<>(capacity);
+    }
+
+    public MaxHeap() {
+        data = new Array<>();
+    }
+
+    public int size() {
+        return data.getSize();
+    }
+
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    private int parent(int index) {
+        if (index == 0)
+            throw new IllegalArgumentException("index-0 doesn't have parent");
+        return (index - 1) / 2;
+    }
+
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+}
